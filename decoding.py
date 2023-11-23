4f12ff7970d9e281671179fc2d50904f6fe0facf
harshadakhorgade <harshadakhorgade9@gmail.com>
2023-11-23 14:12:47 +0530

    completed

--- decoding.py
+++ decoding.py
@@ -1,5 +1,6 @@
 str = input(" enter string :")
 coding = int(input( " 1 for coding and 0 for decoding :"))
+
 coding == True if coding == 1 else False
 words =str.split()
 if (coding):
@@ -14,6 +15,17 @@ if (coding):
                 nw.append(word[::-1])
     print(" ".join(nw))
 
+else:
+    nw =[]
+    for word in words:
+            if len(word) >= 3:
+                newword = word[-4] + word[3:-4] 
+                nw.append(newword)
+            else:
+                nw.append(word[::-1])
+    print(" ".join(nw))
 
+
+     
   
 
