480577b78747746e8eed557079c86a1a6a8b7be2
harshadakhorgade <harshadakhorgade9@gmail.com>
2024-01-31 15:31:32 +0530

    third command

---
+++ Hallo/home/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render,HttpResponse
+
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+# Create your views here.
+def index(request):
+    # so basically render takes 3 things 
+    # 1 index
+    # 2 template means index.html
+    # 3 dictionary which is = context 
+    context= {
+        'variable1':'harshada',
+        'variable2':'mango'
+
+    }
+    return render(request,'index.html',context)
+    # return HttpResponse("Home page")
+
+def about(request):
+    return render(request,'about.html')
+
+def services(request):
+    return render(request,'services.html')
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date = datetime.today())
+        contact.save()
+
+        # it is used to flash the success msg on page
+        # this is type of django messaging ft=ramework search it
+        messages.success(request, 'Your message has been sent!')
+    return render(request, 'contact.html')
+ 
