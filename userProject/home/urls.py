f36221ecb2ab72ecd283e0d7a8c010040caf9773
harshadakhorgade <harshadakhorgade9@gmail.com>
2024-02-01 16:01:42 +0530

    code added

--- userProject/home/urls.py
+++ userProject/home/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from home import views
 
-urlpatterns =[
-    path("", views.index,name='home'),
-    path("login", views.login,name='login'),
-    path("logout", views.logout,name='logout'),
+urlpatterns = [ 
+    path('',views.index, name="home"),
+    path('login',views.loginUser, name="login"),
+    path('logout',views.logoutUser, name="logout"),
 ]
